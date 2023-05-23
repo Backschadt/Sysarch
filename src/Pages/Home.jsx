faad00e17b29f54f3729b2a1c4dac5e58fac60aa
Abel Backschadt <abel.backschadt1@gmail.com>
2023-05-23 09:01:57 +0800

    tuesday

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import {useEffect, useState} from 'react'
+import axios from 'axios';
 
 const Home = () => {
+    const HomePage = () => {
+
+        const[data, setdata] = useState([]);
+        
+
+        useEffect(() =>{
+            axios.get('https://api.publicapis.org/entries')
+            then(response =>{
+                setdata(response.data);
+            });
+        },[])
+    }
+
     return (
         <h1> This is a Home Page :)</h1>      
     )
